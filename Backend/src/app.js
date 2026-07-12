e881a03afbbc808e6a11f1cd9db9ccc49051ef10
codermanan69 <manansahni295@gmail.com>
2026-07-13 00:55:19 +0530

    server frontend in production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -20,18 +20,18 @@ app.use(cors({
     credentials: true
 }));
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Server is running"
-    });
-})
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Server is running"
+//     });
+// })
 
 app.use("/api/auth", authRouter);
 app.use("/api/chats",chatRouter);
 app.use(express.static(path.join(__dirname, "../public")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 export default app
